5cd49ab5a27c976c9b7231210142475541e4750f
Klimov Vadim <vadim.klimov2019.4107@gmailcom>
2025-05-03 22:48:59 +0300

    Start of the youtube project. Typescript, Tailwind, Next configuration has been configured.

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import type { PropsWithChildren } from 'react'
+import { Sidebar } from './sidebar/Sidebar'
+import { Content } from './content/Content'
+
+export function Layout({ children }: PropsWithChildren<unknown>) {
+  return  (
+    <main>
+      <Sidebar />
+      <Content>{children}</Content>
+    </main>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import { COLORS } from "@/constants/colors.constants";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: COLORS,
+    },
+  },
+  plugins: [],
+};
+export default config;
